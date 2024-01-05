b80dd475c1e8761b2b820cae1e9dec2c076331ce
Mehdi Zare <MehdiZare@users.noreply.github.com>
2024-01-04 20:21:01 -0500

    The provided commit message does not correspond to the given code changes. Here is the suggested commit message based on given diff:
    Commit message:
    Comment out text generation script in inference.py
    
    This commit temporarily disables the text generation script in the inference module. This includes the loading of the model, creating a text generation pipeline, and generating texts based on an input prompt.

--- src/inference.py
+++ src/inference.py
@@ -5,9 +5,9 @@ from src.config import settings
 tokenizer = AutoTokenizer.from_pretrained(settings.MODEL_WEIGHT_PATH)
 tokenizer.pad_token = tokenizer.eos_token
 
-model = AutoModelForCausalLM.from_pretrained(settings.MODEL_WEIGHT_PATH)
-
-text_generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
-
-input_prompt = "Today is a beautiful day"
-generated_texts = text_generator(input_prompt, max_length=50, num_return_sequences=1)
+# model = AutoModelForCausalLM.from_pretrained(settings.MODEL_WEIGHT_PATH)
+#
+# text_generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
+#
+# input_prompt = "Today is a beautiful day"
+# generated_texts = text_generator(input_prompt, max_length=50, num_return_sequences=1)
